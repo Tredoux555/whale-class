45259570bc4c4b14b4639cc7c34161e863a074d0
Tredoux555 <tredoux555@gmail.com>
2025-11-11 13:59:56 +0800

    Restore maintenance screen

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ interface Video {
 
 export default function Home() {
   // Set to true to show maintenance screen, false to show normal site
-  const MAINTENANCE_MODE = false;
+  const MAINTENANCE_MODE = true;
   
   const [videos, setVideos] = useState<Video[]>([]);
   const [loading, setLoading] = useState(true);
