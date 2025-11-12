46b6a45ea01f1d561fc32f56c6a996814bc4fd88
Tredoux555 <tredoux555@gmail.com>
2025-11-12 15:42:43 +0800

    Remove unused useRef import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useMemo, useRef } from "react";
+import { useEffect, useState, useMemo } from "react";
 import Link from "next/link";
 import { getProxyVideoUrl } from "@/lib/video-utils";
 
